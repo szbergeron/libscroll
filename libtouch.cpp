b2da658603762257eebe5f32e79d4886683492f6
Sawyer Bergeron <sawyerbergeron@gmail.com>
2019-10-16 22:33:53 -0400

    fix extern issues

--- libtouch.cpp
+++ libtouch.cpp
@@ -1,22 +1,34 @@
-//#include 
 #include <memory>
 #include <ostream>
 #include <vector>
 #include <cstdlib>
 #include <iostream>
+#include <variant>
 #include "libtouch.h"
 
 
 namespace events {
     struct pan_event {
-        enum lscroll_input_source_t source;
         int64_t pan_x;
         int64_t pan_y;
+    };
+
+    struct interrupt_event {
+    };
+
+    struct fling_event {
+    };
+
+    struct event {
+        enum lscroll_input_source_t source;
+        //std::variant<pan_event, fling_event, interrupt_event> content;
     };
 }
 
 
-struct lscroll_scrollviewstate {
+
+
+struct lscroll_scrollview {
     uint64_t content_height = 0;
     uint64_t content_width = 0;
 
@@ -25,30 +37,40 @@ struct lscroll_scrollviewstate {
 
     std::vector<events::pan_event> past_events;
 
-    lscroll_scrollviewstate() {
+    lscroll_scrollview() {
 
     }
-    ~lscroll_scrollviewstate(void) {
+    ~lscroll_scrollview(void) {
 
     }
 };
 
+extern "C" {
+
 struct lscroll_scrollview* lscroll_create_scrollview() {
     struct lscroll_scrollview* sv = new struct lscroll_scrollview;
-
-    sv->state = new struct lscroll_scrollviewstate;
 
     return sv;
 };
 
 void lscroll_destroy_scrollview(struct lscroll_scrollview* sv) {
-    // free subelements
-    if(sv) { // consider removing, passing null here should never occur
-        delete sv->state;
-        delete sv;
-    }
+    delete sv;
 }
 
+void lscroll_set_geometry(
+        struct lscroll_scrollview* handle,
+        uint64_t content_height,
+        uint64_t content_width,
+        uint64_t viewport_height,
+        uint64_t viewport_width
+) {
+    //insert values into header
+    std::cout << "scrollview geometry updated to" << std::endl
+        << "viewport w/h: " << viewport_width << ", " << viewport_height << std::endl
+        << " content w/h: " << content_width << ", " << content_height << std::endl;
+}
+
+/*
 void lscroll_signal_geometry(struct lscroll_scrollview* sv) {
     std::cout << "scrollview geometry updated to" << std::endl
         << "viewport w/h: " << sv->viewport_width << ", " << sv->viewport_height << std::endl
@@ -58,6 +80,8 @@ void lscroll_signal_geometry(struct lscroll_scrollview* sv) {
     sv->state->content_width = sv->content_width;
     sv->state->viewport_height = sv->viewport_height;
     sv->state->viewport_width = sv->viewport_width;
+}
+*/
 }
 
 
--- libtouch.h
+++ libtouch.h
@@ -1,4 +1,5 @@
-#include <cstdint>
+#include <stdint.h>
+#include <stdbool.h>
 /**
  * This library serves as an event interpretation library.
  * To use, you will need to take the raw events you recieve
@@ -11,7 +12,7 @@
 
 /**
  * Example usage:
- *
+ * // TODO update usage example
  * 1. Create some `struct scrollview` locally and pass geometry
  *      and expected behavior as specified in struct
  *
@@ -39,180 +40,218 @@
 
 //NOTE: this library is not yet multithreading safe, but that is TODO
 
-extern "C" {
-
 /**
  * Any of these options can be logical or'ed together
  * and passed to set_options
  */
-enum lscroll_options {
-    LIBSCROLL_IMPRECISE_SCROLLS_SMOOTHLY = 0x1, // controls whether large jumps from imprecise devices (keyboard, clickwheel) should animate smoothly
-};
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-struct lscroll_pan_transform {
-    int64_t x; // x axis pan amount in dp
-    int64_t y; // y axis pan amount in dp
-    bool panned : 1; // only true if a pan event has occurred.
-                 // A transform can be skipped if this is false, otherwise assume
-                 // that a pan has occurred and do a transform of the viewport by
-                 // x and y
-                 //
-                 // also indicates that no further pan or state change
-                 // will occur without adding another event to
-                 // the queue, so any render loop can block safely
-};
+    enum lscroll_options {
+        LIBSCROLL_IMPRECISE_SCROLLS_SMOOTHLY = 0x1, // controls whether large jumps from imprecise devices (keyboard, clickwheel) should animate smoothly
+    };
 
-struct lscroll_scrollview {
-    uint64_t content_height; // height of scrollview content space in dp
-    uint64_t content_width; // width of scrollview content space in dp
-    uint64_t viewport_height; // height of viewport in dp
-    uint64_t viewport_width; // width of viewport in dp
+    /*struct lscroll_pan_transform {
+        int64_t x; // x axis pan amount in dp
+        int64_t y; // y axis pan amount in dp
+        bool panned : 1; // only true if a pan event has occurred.
+                     // A transform can be skipped if this is false, otherwise assume
+                     // that a pan has occurred and do a transform of the viewport by
+                     // x and y
+                     //
+                     // also indicates that no further pan or state change
+                     // will occur without adding another event to
+                     // the queue, so any render loop can block safely
+    };*/
 
-    // x and y axis offset of top-left corner of viewport relative to top-left corner of content
-    // equivalent to using force_pan() after setting geometry
-    //int64_t viewport_initial_x;
-    //int64_t viewport_initial_y;
+    /*struct lscroll_scrollview {
+        uint64_t content_height; // height of scrollview content space in dp
+        uint64_t content_width; // width of scrollview content space in dp
+        uint64_t viewport_height; // height of viewport in dp
+        uint64_t viewport_width; // width of viewport in dp
 
-    // whether to enable overscroll bounce behavior on each edge
-    //bool bounce_bottom: 1;
-    //bool bounce_top: 1;
-    //bool bounce_left: 1;
-    //bool bounce_right: 1;
+        // x and y axis offset of top-left corner of viewport relative to top-left corner of content
+        // equivalent to using force_pan() after setting geometry
+        //int64_t viewport_initial_x;
+        //int64_t viewport_initial_y;
 
-    //TODO: determine if additional "soft boundaries" would be beneficial for overscroll purposes
-    //TODO: allow setting "magnetic" points in a scrollview
+        // whether to enable overscroll bounce behavior on each edge
+        //bool bounce_bottom: 1;
+        //bool bounce_top: 1;
+        //bool bounce_left: 1;
+        //bool bounce_right: 1;
+
+        //TODO: determine if additional "soft boundaries" would be beneficial for overscroll purposes
+        //TODO: allow setting "magnetic" points in a scrollview
 
 
-    // NOTE: any variables past this point should be considered opaque and implementation defined
+        // NOTE: any variables past this point should be considered opaque and implementation defined
 
-    struct lscroll_scrollviewstate* state; // handle used for internally tracking state of a given scrollview by libtouch
-};
+        struct lscroll_scrollviewstate* state; // handle used for internally tracking state of a given scrollview by libtouch
+    };*/
 
-/**
- * Do initialization tasks for and return a handle to a new scrollview
- *
- * Default geometry will be used for this variant, and can be updated
- * with signal_geometry() on a modified scrollview
- */
-struct lscroll_scrollview* lscroll_create_scrollview();
+    /** Set the geometry for the current scrollview */
+    void lscroll_set_geometry(
+            struct lscroll_scrollview* handle,
+            uint64_t content_height,
+            uint64_t content_width,
+            uint64_t viewport_height,
+            uint64_t viewport_width
+    );
 
-///**
-// * Create a new scrollview and simultaneously default
-// * initialize the geometry of the scrollview to that
-// * of the passed scrollview
-// */
-//struct scrollview* create_scrollview(struct scrollview view);
+    /**
+     * Returns true if a pan is ongoing and rendering should continue
+     * (render thread should not block)
+     *
+     * A scrollview may be active still even if pan amount is zero,
+     * for instance if a scrollview is briefly balanced on a
+     * magnetic boundary edge
+     */
+    bool lscroll_query_pan_active(struct lscroll_scrollview* handle);
 
-/**
- * Tears down and frees the referenced scrollview
- *
- * The handle passed here should be considered invalid
- * after this function has been called
- */
-void lscroll_destroy_scrollview(lscroll_scrollview* view);
+    /**
+     * Do initialization tasks for and return a handle to a new scrollview
+     *
+     * Default geometry will be used for this variant, and can be updated
+     * with signal_geometry() on a modified scrollview
+     */
+    struct lscroll_scrollview* lscroll_create_scrollview();
 
-/**
- * Signals that geometry for the referenced scrollview
- * has been changed and should be updated
- */
-void lscroll_signal_geometry(struct scrollview* view);
+    ///**
+    // * Create a new scrollview and simultaneously default
+    // * initialize the geometry of the scrollview to that
+    // * of the passed scrollview
+    // */
+    //struct scrollview* create_scrollview(struct scrollview view);
 
-/**
- * Allows forcing a relative scroll by x, y dp in the current scrollview
- *
- * Example use case: user uses a keyboard shortcut to jump down by a page
- */
-void lscroll_force_pan(int64_t x_dp, int64_t y_dp);
+    /**
+     * Tears down and frees the referenced scrollview
+     *
+     * The handle passed here should be considered invalid
+     * after this function has been called
+     */
+    void lscroll_destroy_scrollview(struct lscroll_scrollview* handle);
 
-/**
- * Allows forcing a scroll to position x, y dp in the current scrollview
- *
- * Example use case: user jumps to an absolute line number in a text editor
- */
-void lscroll_force_jump(int64_t x_dp, int64_t y_dp);
+    /**
+     * Allows forcing a relative scroll by x, y dp in the current scrollview
+     *
+     * Example use case: user uses a keyboard shortcut to jump down by a page
+     */
+    void lscroll_force_pan(
+            struct lscroll_scrollview* handle,
+            int64_t x_dp,
+            int64_t y_dp
+    );
 
-/**
- * Sets how long the average frame is as well as how far
- * in the future to predict a pan. This allows us to slightly
- * overshoot any pan to minimize percieved lag
- */
-void lscroll_set_predict(float ms_to_vsync, float ms_avg_frametime);
+    /**
+     * Allows forcing a scroll to position x, y dp in the current scrollview
+     *
+     * Example use case: user jumps to an absolute line number in a text editor
+     */
+    void lscroll_force_jump(
+            struct lscroll_scrollview* handle,
+            int64_t x_dp,
+            int64_t y_dp
+    );
 
-/**
- * WARN: the following get_[...]() functions should only be called after a call to mark_frame()
- */
-int64_t lscroll_get_pan_x(); // gets x component of current pan. WARN: mutates internal state: clears x axis event buffer
-int64_t lscroll_get_pan_y(); // gets y component of current pan. WARN: mutates internal state: clears y axis event buffer
+    /**
+     * Sets how long the average frame is as well as how far
+     * in the future to predict a pan. This allows us to slightly
+     * overshoot any pan to minimize percieved lag
+     */
+    void lscroll_set_predict(
+            struct lscroll_scrollview* handle,
+            float ms_to_vsync,
+            float ms_avg_frametime
+    );
 
-int64_t lscroll_get_pos_x(); // gets absolute x position of current viewport into content
-int64_t lscroll_get_pos_y(); // gets absolute y position of current viewport into content
+    /**
+     * WARN: the following get_[...]() functions should only be called after a call to mark_frame()
+     */
 
-///**
-// * set_input_source should be properly used always, since
-// * if input is assumed to be a touchpad and turns out to
-// * be a touchscreen, an acceleration curve will be applied
-// * which will desynchronize touch point and panning
-// *
-// * scroll_natural also only applies to 
-// */
-//void set_scale_factor(float x_factor, float y_factor); // normalization factor for quirky devices
+    /** gets x component of current pan. WARN: mutates internal state: clears x axis event buffer */
+    int64_t lscroll_get_pan_x(struct lscroll_scrollview* handle);
 
-enum lscroll_input_source_t {
-    LSCROLL_SOURCE_UNDEFINED, // acts identically to PASSTHROUGH_KINETIC,
-               // only use when no hint is available as
-               // to what input source is
-    LSCROLL_SOURCE_TOUCHSCREEN,
-    LSCROLL_SOURCE_TOUCHPAD,
-    LSCROLL_SOURCE_MOUSEWHEEL,
-    LSCROLL_SOURCE_MOUSEWHEEL_PRECISE,
-    LSCROLL_SOURCE_PASSTHROUGH, // use for inputs that have their own drivers
-                 // handling any acceleration curves
-                 // or overshoot; disables any
-                 // input processing here, only sum pan distance
-                 // Examples: TrackPoint, trackball, mousekeys
-    LSCROLL_SOURCE_PASSTHROUGH_KINETIC, // use as prior, but keep
-                         // kinetic scrolling after scroll_release event
-};
+    /** gets y component of current pan. WARN: mutates internal state: clears y axis event buffer */
+    int64_t lscroll_get_pan_y();
 
-/**
- * should be called before any add_scroll_[...]() function call for a given device,
- * as any scroll event call is interpreted as coming from the last input source set
- */
-void lscroll_set_input_source(enum lscroll_input_source_t input_source);
+    /** gets absolute x position of current viewport into/relative to content */
+    int64_t lscroll_get_pos_x();
 
-int lscroll_add_scroll(
-    int64_t motion_x, // x axis motion reported by device
-    int64_t motion_y // y axis motion reported by device
-);
+    /** gets absolute y position of current viewport into/relative to content */
+    int64_t lscroll_get_pos_y();
 
-/**
- * similar to add_scroll(motion_x, motion_y),
- * use for when input device delivers axes
- * as separate events
- */
-int lscroll_add_scroll_x(int64_t motion_x);
-int lscroll_add_scroll_y(int64_t motion_y);
+    ///**
+    // * set_input_source should be properly used always, since
+    // * if input is assumed to be a touchpad and turns out to
+    // * be a touchscreen, an acceleration curve will be applied
+    // * which will desynchronize touch point and panning
+    // *
+    // * scroll_natural also only applies to 
+    // */
+    //void set_scale_factor(float x_factor, float y_factor); // normalization factor for quirky devices
 
-/**
- * analogous to "was scrolling kinetically,
- * until user put two fingers back on touchpad"
- */
-void lscroll_add_scroll_interrupt();
+    enum lscroll_input_source_t {
+        LSCROLL_SOURCE_UNDEFINED, // acts identically to PASSTHROUGH_KINETIC,
+                   // only use when no hint is available as
+                   // to what input source is
+        LSCROLL_SOURCE_TOUCHSCREEN,
+        LSCROLL_SOURCE_TOUCHPAD,
+        LSCROLL_SOURCE_MOUSEWHEEL,
+        LSCROLL_SOURCE_MOUSEWHEEL_PRECISE,
+        LSCROLL_SOURCE_PASSTHROUGH, // use for inputs that have their own drivers
+                     // handling any acceleration curves
+                     // or overshoot; disables any
+                     // input processing here, only sum pan distance
+                     // Examples: TrackPoint, trackball, mousekeys
+        LSCROLL_SOURCE_PASSTHROUGH_KINETIC, // use as prior, but keep
+                             // kinetic scrolling after scroll_release event
+    };
 
-/**
- * triggers kinetic scrolling,
- * last event to be sent during
- * a "flick" action
- */
-void lscroll_add_scroll_release();
+    /**
+     * should be called before any add_scroll_[...]() function call for a given device,
+     * as any scroll event call is interpreted as coming from the last input source set
+     */
+    void lscroll_set_input_source(enum lscroll_input_source_t input_source);
 
-/**
- * Call this as late in the rendering pipeline as possible before asking
- * for current pan/geometry.
- *
- * Internally this takes a snapshot of the proposed pan amount
- * and locks those numbers until the next call to mark_frame()
- */
-void lscroll_mark_frame();
+    /** Add some pan event to the scrollview referenced by handle */
+    int lscroll_add_scroll(
+            struct lscroll_scrollview* handle,
+            int64_t motion_x, // x axis motion reported by device
+            int64_t motion_y // y axis motion reported by device
+    );
 
+    /**
+     * similar to add_scroll(motion_x, motion_y),
+     * use for when input device delivers axes
+     * as separate events
+     */
+    int lscroll_add_scroll_x(struct lscroll_scrollview* handle, int64_t motion_x);
+    int lscroll_add_scroll_y(struct lscroll_scrollview* handle, int64_t motion_y);
+
+    /**
+     * analogous to "was scrolling kinetically,
+     * until user put two fingers back on touchpad"
+     */
+    void lscroll_add_scroll_interrupt(struct lscroll_scrollview* handle);
+
+    /**
+     * triggers kinetic scrolling,
+     * last event to be sent during
+     * a "flick" action
+     */
+    void lscroll_add_scroll_release(struct lscroll_scrollview* handle);
+
+    /**
+     * Call this as late in the rendering pipeline as possible before asking
+     * for current pan/geometry.
+     *
+     * Internally this takes a snapshot of the proposed pan amount
+     * and locks those numbers until the next call to mark_frame()
+     */
+    void lscroll_mark_frame(struct lscroll_scrollview* handle);
+
+#ifdef __cplusplus
 }
+#endif
